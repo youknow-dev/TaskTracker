5fe065400d5feb6d34d6a1318953275f731e2d10
youknow-dev <youknow.backend@yahoo.com>
2026-05-19 21:11:21 -0400

    removed tracked temp and build files. Added gitignore

--- TaskTracker/obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
+++ TaskTracker/obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfe56e33c1c6b2fba418208018f478bf6953820")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
